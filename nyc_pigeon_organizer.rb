e1253921e1370f168bf496807ceb935ec9df8eb8
Trevor Low <tlow94@uw.edu>
2020-04-10 17:57:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,9 @@
 require 'pry'
 def nyc_pigeon_organizer (data)
 new_hash = {}
+data.each do |key, value|
+  value.each do |new_key, name_array|
+    binding.pry
+    name_array.each do |name|
+    
 end
